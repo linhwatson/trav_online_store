77dc0fab7b3307e7d3381db672bce758a1148108
Linh <linhscholar71@gmail.com>
2023-07-12 05:07:16 -0400

    add routes to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./components/Details";
+import DropDown from "./components/DropDown";
 
 const App = () => {
-  return <h2>Online Store</h2>;
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to='/'>Online Store</Link>
+      </header>
+      <Routes>
+        <Route path='/' element={<DropDown />} />
+        <Route path='/details/:id' element={<Details />} />
+      </Routes>
+    </BrowserRouter>
+  )
 };
 
 const container = document.getElementById("root");
