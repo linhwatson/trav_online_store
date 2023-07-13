7e2d997bc5b80a05aa88e4bda14aa6f7d8873381
Linh <linhscholar71@gmail.com>
2023-07-12 21:15:04 -0400

    render items

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,5 +1,19 @@
-const Item = () => {
-  return<h2>Item</h2>
+import { Link } from 'react-router-dom';
+
+const Item = ( {id, name, price, category, image }) => {
+  let img = image;
+
+  return (
+    <Link to={`/details/${id}`} className='item'>
+      <div className='image-container'>
+        <img src={img} alt={name}/>
+      </div>
+      <div className='info'>
+        <h1>{name}</h1>
+        <h2>${price} - {category}</h2>
+      </div>
+    </Link>
+  )
 };
 
 export default Item;
--- src/components/ItemsList.jsx
+++ src/components/ItemsList.jsx
@@ -1,5 +1,25 @@
+import Item from "./Item";
+
 const ItemsList = ({ items }) => {
-  return <h2>Render a list of food items</h2>;
+  return (
+    <div>
+      {!items.length ? (
+        <h1>No Food Items Found!</h1>
+      ) : (
+        items.map((item) => (
+          <Item
+            key={item.id}
+            id={item.id}
+            name={item.name}
+            category={item.category}
+            description={item.description}
+            price={item.price}
+            image={item.image}
+          />
+        ))
+      )}
+    </div>
+  )
 };
 
 export default ItemsList;
