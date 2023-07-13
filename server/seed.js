19c84d499c3f1c8d7c472a32bd4b12745c1000aa
Linh <linhscholar71@gmail.com>
2023-07-13 08:10:36 -0400

    seeding MongoDB

--- server/seed.js
+++ server/seed.js
@@ -1,1 +1,8 @@
-const { Item } = require( )
+const { Item } = require("./db.js");
+const itemData = require("../product_list.json");
+
+Item.deleteMany({})
+  .then(() => Promise.all(itemData.map((item) => Item.create(item))))
+  .then(() => console.log("Item collection has been reset!"))
+  .catch((err) => console.log("Error resetting Item collection: ", err))
+  .then(() => process.exit());
