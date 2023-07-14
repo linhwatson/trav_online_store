69d95ed65c4173622878e2f7d41ac8bdd09e20d4
Linh <linhscholar71@gmail.com>
2023-07-14 10:36:42 -0400

    replace mockdata in Details with data from db

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,19 +1,33 @@
 import { useParams, useNavigate } from 'react-router-dom';
-import mockData from '../../data/mockData.json';
+import { useQuery } from '@tanstack/react-query';
+import FetchItem from './FetchItem';
 
 const Details = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
-  const item = mockData[id];
+  const result = useQuery(['details', id], FetchItem);
+
+  if (result.isLoading) {
+    return (
+      <div className='loading-pane'>
+        <h2 className='loader'>🦔</h2>
+      </div>
+    )
+  }
+
+  const item = result?.data;
 
   return (
     <div>
-      <h1>{item.name}</h1>
-      <h2>
-        ${item.price}
-        <p>{item.description}</p>
-      </h2>
+      <h1>{item.product_name}</h1>
+      <h2>${item.price}</h2>
+      <h3>
+        <p>Popularity: {item.popularity}</p>
+        <p>Durability: {item.durability}</p>
+        <p>Days Until Expiration: {item.daysuntilexpiration}</p>
+        <p>Barcode: {item.barcode}</p>
+      </h3>
       <button>
         Add to Cart
       </button>
