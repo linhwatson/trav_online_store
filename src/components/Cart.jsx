d6f1539dde7ec33ee8c325cf3a9df3bd0db1bfc0
Linh <linhscholar71@gmail.com>
2023-07-17 00:42:42 -0400

    add Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,35 @@
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import Item from './Item';
+import { useNavigate } from 'react-router-dom';
+
 const Cart = () => {
-  return <h2>Cart</h2>;
+  const navigate = useNavigate();
+  const cartList = useSelector((store) => store.cartListReducer);
+
+  useEffect(() => {
+    console.log('cartList', cartList);
+    //run script for Predictive Model
+  }, []);
+
+  return (
+    <div>
+      {!cartList.length ? (
+        <h1>No Food Items Found!</h1>
+      ) : (
+        cartList.map((item) => (
+          <Item
+            key={item.unit_id}
+            id={item.unit_id}
+            product_name={item.product_name}
+            price={item.price}
+            popularity={item.popularity}
+          />
+        ))
+      )}
+      <button onClick={() => navigate("/payment")}>Checkout</button>
+    </div>
+  );
 };
 
 export default Cart;
