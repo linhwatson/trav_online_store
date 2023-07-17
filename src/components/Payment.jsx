428ad84ac28d48f2e263631cdfea403c686fbc16
Linh <linhscholar71@gmail.com>
2023-07-17 00:56:07 -0400

    modify Payment component

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -3,7 +3,52 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 const Payment = () => {
+  const [address, setAddress] = useState("");
+  const [payment, setPayment] = useState("");
 
+  const cartList = useSelector((store) => store.cartListReducer);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //TODO replace console.log with axios POST request
+    console.log("Address: ", address, "Payment: ", payment, "Cart: ", cartList);
+     setAddress("");
+     setPayment("");
+
+     dispatch({
+       type: "CLEAR_CART",
+     });
+     navigate("/");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Address:
+          <input
+            type="text"
+            placeholder="Address"
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+          />
+        </label>
+        <label>
+          Payment Information:
+          <input
+            type="text"
+            placeholder="Payment"
+            value={payment}
+            onChange={(event) => setPayment(event.target.value)}
+          />{" "}
+        </label>
+        <button type="submit">Final Checkout</button>
+      </form>
+    </div>
+  );
 };
 
 export default Payment;
